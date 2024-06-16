d7ca933759eafb04d2b647da7946ee43c219c640
Andarg Worku <97668901+AndargWorku@users.noreply.github.com>
2024-06-16 12:52:44 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ const app = express()
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://gebeya-p9r3-6zo2g4oa4-andargworkus-projects.vercel.app/',
-        'https://gebeya-p9r3-6zo2g4oa4-andargworkus-projects.vercel.app'
+        'https://gebeya-p9r3.vercel.app/',
+        'https://gebeya-p9r3.vercel.app'
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
    
