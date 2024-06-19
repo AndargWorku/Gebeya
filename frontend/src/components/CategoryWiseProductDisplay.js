7dbdbee36b17b45510a0c165e1dfd5f1f6ae19d0
Andarg01 <workuandarg01@gmail.com>
2024-06-19 02:02:44 -0700

    update something

--- frontend/src/components/CategoryWiseProductDisplay.js
+++ frontend/src/components/CategoryWiseProductDisplay.js
@@ -55,10 +55,10 @@ const CategroyWiseProductDisplay = ({category, heading}) => {
                                 </div>
                                 <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200'></h2>
-                                    <p className='capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200  py-2'></p>
+                                    <p className='capitalize text-yellow-700 p-1 animate-pulse rounded-full bg-slate-200  py-2'></p>
                                     <div className='flex gap-3'>
                                         <p className='text-yellow-800 font-medium p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2'></p>
-                                        <p className='text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2'></p>
+                                        <p className='text-yellow-700 line-through p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2'></p>
                                     </div>
                                     <button className='text-sm  text-white px-3  rounded-full bg-slate-200  py-2 animate-pulse'></button>
                                 </div>
@@ -74,10 +74,10 @@ const CategroyWiseProductDisplay = ({category, heading}) => {
                                 </div>
                                 <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black'>{product?.productName}</h2>
-                                    <p className='capitalize text-slate-500'>{product?.category}</p>
+                                    <p className='capitalize text-yellow-700'>{product?.category}</p>
                                     <div className='flex gap-3'>
                                         <p className='text-yellow-800 font-medium'>{ displayUSDCurrency(product?.sellingPrice) }</p>
-                                        <p className='text-slate-500 line-through'>{ displayUSDCurrency(product?.price)  }</p>
+                                        <p className='text-yellow-700 line-through'>{ displayUSDCurrency(product?.price)  }</p>
                                     </div>
                                     <button className='text-sm bg-yellow-800 hover:bg-yellow-900 text-white px-3 py-0.5 rounded-full' onClick={(e)=>handleAddToCart(e,product?._id)}>Add to Cart</button>
                                 </div>
